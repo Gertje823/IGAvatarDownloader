b9d4f073ced3c3b391d1df04870a73ac5e27d61d
Gertje823 <36937387+Gertje823@users.noreply.github.com>
2021-04-25 12:10:25 +0200

    Now working again

--- downloader.py
+++ downloader.py
@@ -14,15 +14,12 @@ date = today.strftime("%d-%m-%Y")
 i = 1
 sessionid ="YOUR_SESSIONID_HERE"
 
-for user in users:
-        user = user.replace("\n","")
+for userid in users:
+        userid = userid.replace("\n","")
         try:
                 if i < 20:
                         i+=1
-                        r=requests.get(f"https://www.instagram.com/{user}?__a=1", headers={"User-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_3 like Mac OS X) AppleWebKit/603.3.8 (KHTML, like Gecko) Mobile/14G60 Instagram 12.0.0.16.90 (iPhone9,4; iOS 10_3_3; en_US; en-US; scale=2.61; gamut=wide; 1080x1920)"})
-                        data = r.json()
-                        userid = data['graphql']['user']['id']
-                            
+
                         r=requests.get(f"https://i.instagram.com/api/v1/users/{userid}/info/", headers={"Cookie":f"sessionid={sessionid};", "User-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_3 like Mac OS X) AppleWebKit/603.3.8 (KHTML, like Gecko) Mobile/14G60 Instagram 12.0.0.16.90 (iPhone9,4; iOS 10_3_3; en_US; en-US; scale=2.61; gamut=wide; 1080x1920)"})
                         data = r.json()
                         username = data['user']['username']
